10b2d6bd3a0766dd8e3de5e167c6890b6fbd216f
rahulsd380 <rahulsd380@gmail.com>
2025-08-29 09:26:00 +0600

    Added crate blog

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -6,7 +6,6 @@ import ErrorHandler from "../utils/errorhandler.js";
 import { processTags } from "../utils/tags.js";
 import { deleteImage, uploadImage } from "../utils/uploadImage.js";
 import categoryModel from "../models/category.model.js";
-import { processBlogsWithSubCategory } from "./category.controller.js";
 
 export const createBlog = catchAsyncErrors(async (req, res, next) => {
   const { title, metaDescription, content, tags } = req.body;
@@ -46,21 +45,17 @@ export const getAllBlogs = catchAsyncErrors(async (req, res, next) => {
   const resultPerPage = 15;
   const currentPage = Number(req.query.page) || 1;
 
-  const baseQuery = blogModel
-    .find()
-    .populate("author", "full_name")
-    .populate("category", "name subCategory");
+  // Base query without populate
+  const baseQuery = blogModel.find();
 
   const apiFeature = new ApiFeatures(baseQuery, req.query).search().filter();
 
+  // Count for pagination
   const filteredBlogsCount = await blogModel.countDocuments(apiFeature.query);
-
   apiFeature.pagination(resultPerPage);
 
+  // Fetch blogs
   const blogs = await apiFeature.query;
-
-  // Map over the blogs to replace the subCategory ID with the actual subcategory name
-  const processedBlogs = processBlogsWithSubCategory(blogs);
   const totalBlogsCount = await blogModel.estimatedDocumentCount();
 
   res.status(200).json({
@@ -70,26 +65,26 @@ export const getAllBlogs = catchAsyncErrors(async (req, res, next) => {
     resultPerPage,
     currentPage,
     totalPages: Math.ceil(filteredBlogsCount / resultPerPage),
-    data: processedBlogs,
+    data: blogs,
   });
 });
+
 
 export const singleBlog = catchAsyncErrors(async (req, res, next) => {
   const blog = await blogModel
     .findById(req.params.id)
-    .populate("author", "full_name")
-    .populate("category", "name subCategory");
+    .populate("author", "full_name");
 
   if (!blog) {
     return next(new ErrorHandler("Blog not found", 404));
   }
-  const processedBlog = processBlogsWithSubCategory(blog);
 
   res.status(200).json({
     success: true,
-    data: processedBlog,
+    data: blog,
   });
 });
+
 
 export const deleteBlog = catchAsyncErrors(async (req, res, next) => {
   const blog = await blogModel.findById(req.params.id);
@@ -114,12 +109,11 @@ export const deleteBlog = catchAsyncErrors(async (req, res, next) => {
     message: `Blog deleted successfully`,
   });
 });
+
 export const updateBlog = catchAsyncErrors(async (req, res, next) => {
   const { id } = req.params;
-  const { title, metaDescription, content, category, subCategory, tags } =
-    req.body;
+  const { title, metaDescription, content, tags } = req.body;
   const authorId = res.locals.admin.id;
-  console.log(subCategory, "subCategory");
 
   const blog = await blogModel.findById(id);
   if (!blog) {
@@ -136,48 +130,8 @@ export const updateBlog = catchAsyncErrors(async (req, res, next) => {
   if (title) updateFields.title = title;
   if (metaDescription) updateFields.metaDescription = metaDescription;
   if (content) updateFields.content = content;
+  if (tags) updateFields.tags = processTags(tags);
 
-  if (category || subCategory) {
-    let categoryData;
-    if (category) {
-      categoryData = await categoryModel.findById(category);
-      if (!categoryData) {
-        return next(new ErrorHandler("Category not found", 404));
-      }
-      updateFields.category = category;
-    }
-
-    if (subCategory) {
-      if (!category) {
-        return next(
-          new ErrorHandler(
-            "Please provide category while updating subcategory",
-            400
-          )
-        );
-      }
-
-      // Ensure the category data is available and subCategory exists in the category
-      const subCategoryData = categoryData.subCategory.find(
-        (sub) => sub._id.toString() === subCategory.toString()
-      );
-
-      if (!subCategoryData) {
-        return next(
-          new ErrorHandler(
-            "Subcategory not found in the selected category",
-            404
-          )
-        );
-      }
-
-      updateFields.subCategory = subCategory; // Add subcategory to updateFields
-    }
-  }
-
-  if (tags) {
-    updateFields.tags = processTags(tags);
-  }
   if (req.file) {
     if (blog.thumbnail) {
       await deleteImage(blog.thumbnail.fileId);
@@ -190,7 +144,7 @@ export const updateBlog = catchAsyncErrors(async (req, res, next) => {
     updateFields.thumbnail = thumbnail;
   }
 
-  if (Object.keys(updateFields).length === 0 && !req.file) {
+  if (Object.keys(updateFields).length === 0) {
     return next(new ErrorHandler("No fields provided for update", 400));
   }
 
@@ -205,80 +159,3 @@ export const updateBlog = catchAsyncErrors(async (req, res, next) => {
     data: updatedBlog,
   });
 });
-
-// export const updateBlog = catchAsyncErrors(async (req, res, next) => {
-//   const { id } = req.params;
-//   const { title, metaDescription, content, category, tags } = req.body;
-//   const authorId = res.locals.admin.id;
-
-//   const blog = await blogModel.findById(id);
-//   if (!blog) {
-//     return next(new ErrorHandler("Blog not found", 404));
-//   }
-
-//   if (blog.author.toString() !== authorId) {
-//     return next(
-//       new ErrorHandler("You are not authorized to update this blog", 403)
-//     );
-//   }
-
-//   const updateFields = {};
-
-//   if (title) updateFields.title = title;
-//   if (metaDescription) updateFields.metaDescription = metaDescription;
-//   if (content) updateFields.content = content;
-
-//   if (category) {
-//     let categoryObj;
-//     try {
-//       categoryObj =
-//         typeof category === "string" ? JSON.parse(category) : category;
-//     } catch (error) {
-//       return next(new ErrorHandler("Invalid category format", 400));
-//     }
-
-//     if (categoryObj.name && categoryObj.subCategory) {
-//       updateFields.category = {
-//         name: categoryObj.name,
-//         ...(categoryObj.subCategory && { subCategory: categoryObj.subCategory }),
-//       };
-//     } else {
-//       return next(new ErrorHandler("Please provide both category name and subcategory while updating!", 400));
-//     }
-//   }
-
-//   if (tags) {
-//     const modifiedTags = tags[0]
-//       .split(",")
-//       .map((str) => str.trim().replace(/(^"|"$)/g, ""));
-//     updateFields.tags = modifiedTags;
-//   }
-
-//   if (req.file) {
-//     const file = req.file;
-//     if (blog.thumbnail) {
-//       await deleteImage(blog.thumbnail.fileId);
-//     }
-//     const thumbnail = await uploadImage(
-//       getDataUri(file).content,
-//       getDataUri(file).fileName,
-//       "blog-thumbnails"
-//     );
-//     updateFields.thumbnail = thumbnail;
-//   }
-
-//   if (Object.keys(updateFields).length === 0 && !req.file) {
-//     return next(new ErrorHandler("No fields provided for update", 400));
-//   }
-
-//   const updatedBlog = await blogModel.findByIdAndUpdate(id, updateFields, {
-//     new: true,
-//     runValidators: true,
-//   });
-
-//   res.status(200).json({
-//     success: true,
-//     message: "Blog updated successfully",
-//     data: updatedBlog,
-//   });
-// });
